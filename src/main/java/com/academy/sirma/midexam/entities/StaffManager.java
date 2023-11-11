3f5b7e4b37a6aa356b8464c78649dc0166d49a34
GorgiBg <georgid@orbisds.com>
2023-11-11 14:49:27 +0200

    add functionality to fire, search , list employees, and save

--- src/main/java/com/academy/sirma/midexam/entities/StaffManager.java
+++ src/main/java/com/academy/sirma/midexam/entities/StaffManager.java
@@ -3,7 +3,12 @@ package com.academy.sirma.midexam.entities;
 import com.academy.sirma.midexam.constants.StringConstants;
 import com.academy.sirma.midexam.services.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class StaffManager implements Manager{
 
@@ -30,25 +35,106 @@ public class StaffManager implements Manager{
                 editEmployee(commandLine[1]);
                 break;
             case "FIRE":
-                //fireEmployee();
+                fireEmployee(commandLine[1]);
                 break;
-            case "SEARCH ID":
-                //searchId();
+            case "SEARCH":
+                switch (commandLine[1]) {
+                    case "Id" -> searchId(commandLine[2]);
+                    case "Department" -> searchDepartment(commandLine[2]);
+                    case "Name" -> searchName(commandLine[2]);
+                    default -> System.out.println("Incorrect choice for search criteria.");
+                }
                 break;
-            case "SEARCH DEPARTMENT":
-                //searchDepartment();
+            case "LIST":
+                listEmployees();
                 break;
-            case "SEARCH NAME":
-                //searchName();
-                break;
-            case "LIST EMPLOYEES":
-                //listEmployees();
-                break;
-            case "SAVE & EXIT":
-                //saveAndExit();
+            case "SAVE":
+                saveAndExit();
                 break;
             default:
                 System.out.println("Invalid command");
+        }
+    }
+
+    private void saveAndExit() {
+        try {
+            service.saveData(new ArrayList<>(company.getEmployees().values()));
+            System.out.println("Data saved successfully. Exiting...");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("Error saving data. Please check the file and try again.");
+        }
+    }
+
+    private void listEmployees() {
+        Map<Integer, Employee> employees = company.getEmployees();
+
+        if (employees.isEmpty()) {
+            System.out.println("No employees in the company.");
+        } else {
+            System.out.println("List of all employees:");
+            employees.values().forEach(System.out::println);
+        }
+    }
+
+    private void searchName(String name) {
+        // Filter employees by name
+        List<Employee> employeesWithName = company.getEmployees().values().stream()
+            .filter(employee -> employee.getName().equalsIgnoreCase(name))
+            .toList();
+
+        if (employeesWithName.isEmpty()) {
+            System.out.println("No employees found with the name: " + name);
+        } else {
+            System.out.println("Employees with the name " + name + ":");
+            employeesWithName.forEach(System.out::println);
+        }
+    }
+
+    private void searchDepartment(String department) {
+
+        // Filter employees by department
+        List<Employee> employeesInDepartment = company.getEmployees().values().stream()
+            .filter(employee -> employee.getDepartment().equalsIgnoreCase(department))
+            .toList();
+
+        if (employeesInDepartment.isEmpty()) {
+            System.out.println("No employees found in department: " + department);
+        } else {
+            System.out.println("Employees in department " + department + ":");
+            employeesInDepartment.forEach(System.out::println);
+        }
+    }
+
+    private void searchId(String inputId) {
+        try {
+            int employeeId = Integer.parseInt(inputId);
+            if (!company.getEmployees().containsKey(employeeId)) {
+                System.out.println("Employee not found with ID: " + employeeId);
+            } else {
+                Employee foundEmployee = company.getEmployees().get(employeeId);
+                System.out.println("Employee found with ID " + employeeId + ":");
+                System.out.println(foundEmployee);
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid integer value for Employee ID.");
+        }
+    }
+
+    private void fireEmployee(String inputId) {
+        try {
+            int employeeId = Integer.parseInt(inputId);
+            if (!company.getEmployees().containsKey(employeeId)) {
+                System.out.println("Employee not found with ID: " + employeeId);
+                return;
+            }
+            company.getEmployees().remove(employeeId);
+
+            System.out.println("Employee with ID " + employeeId + " has been fired.");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid numeric value for Employee ID.");
         }
     }
 
